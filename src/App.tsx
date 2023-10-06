f3719feb754ac643fee4b63a9743d0e731ae7a99
dannymunozramirez <danny.munoz.ramirez@gmail.com>
2023-10-06 11:07:17 -0400

    added IBM React Carbon lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './index.scss';
+import { Button } from '@carbon/react';
 
 function App() {
   return (
     <div>
       Test
+      <br/>
+      <br/>
+      <br/>
+      <Button>Test Button</Button>
     </div>
   );
 }
